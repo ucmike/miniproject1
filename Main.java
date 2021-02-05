3381f3195634ab250406c3d2f83459f27ce4eb64
Mike Seitz <seitzme@mail.uc.edu>
2021-02-05 00:29:32 +0000

    created and tested the playGame method.

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 /*
 Mini Project 1
 Group #1: Michael Seitz, YourPartnerName1, YourPartnerName2, YourPartnerName3
@@ -8,15 +9,41 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-     Scanner s = new Scanner(System.in);
+    Scanner s = new Scanner(System.in);
     int userNum;
- 
+
     System.out.println("Welcome to the guessing game! \nPlease enter any positive whole number.:");
-    userNum = s.nextInt(); //the number we are starting with.
+    userNum = s.nextInt(); // the number we are starting with.
     Random r = new Random();
-    int randomNum = r.nextInt(userNum)+1;
+    int randomNum = r.nextInt(userNum) + 1;
 
     System.out.println("A random number to guess has been generated.");
-    System.out.println("Please guess a number between 0 and " +userNum);
+
+    playGame(randomNum, userNum);
+  }
+
+  static void playGame(int toGuess, int userNum1) {
+    Scanner t = new Scanner(System.in);
+    int userGuess;
+    System.out.println("Please guess a number between 0 and " + userNum1);
+    userGuess = t.nextInt();
+    int numTries = 1;
+
+    while (numTries < 10 && userGuess != toGuess) {
+      if (userGuess < toGuess) {
+        System.out.println("Your guess is too low. Guess higher. \nPlease enter your new guess:");
+        numTries++;
+        userGuess = t.nextInt();
+      } else if (userGuess > toGuess) {
+        System.out.println("Your guess is too high. Guess lower. \nPlease enter your new guess:");
+        numTries++;
+        userGuess = t.nextInt();
+
+      }
+    }
+
+    if (userGuess == toGuess) {
+      System.out.println("GREAT YOU WIN. It took " + numTries + " attempts");
+    }
   }
 }
